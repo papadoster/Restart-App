f90b03104823aae249c569017e18f9d1fa0c14d6
Aleksandr Karpov <papadoster13@gmail.com>
2023-02-08 23:05:02 +0100

    Initial Commit

---
+++ Restart-App/Restart_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_AppApp.swift
+//  Restart-App
+//
+//  Created by Александр Карпов on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Restart_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
